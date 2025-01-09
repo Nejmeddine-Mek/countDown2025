cc9cfd66c8b736cb95e5685b0c3ed00a1b7db3af
nejem Eddine Mekentichi <83135662+Nejmeddine-Mek@users.noreply.github.com>
2025-01-09 23:50:59 +0100

    new Update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const deadLine = new Date("2025-01-01T00:00:00")
+const deadLine = new Date("2026-01-01T00:00:00")
+const startTime = new Date("2025-01-01T00:00:00")
+
 const counter = Array.from(document.querySelectorAll("h1"))
+const percentage = document.getElementById("percentage")
+const timePassed = document.getElementById("timePassed")
 function updateTimer(){
     const curTime = new Date()
+    const totalTime = deadLine - startTime
+    const elapsedTime = curTime - startTime
+    console.log(totalTime, elapsedTime)
+    const percent = Math.max(0, elapsedTime / totalTime * 100)
+    console.log(percent)
+    percentage.style.width = `${Math.min(percent,100)}%` 
+    timePassed.textContent = `${percent.toFixed(2)}%`
     let seconds, minutes, hours, days
     seconds = Math.floor((deadLine - curTime) / 1000)
     if(seconds > 0){
@@ -20,7 +31,7 @@ function updateTimer(){
         const container = document.getElementById("countDown")
         container.innerHTML = ""
         let newMsg = document.createElement("h1")
-        newMsg.textContent = "Welcome to 2025!🎊🎉"
+        newMsg.textContent = "Welcome to 2026!🎊🎉"
         container.append(newMsg)
     }
 }
